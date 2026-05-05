6b86c6ce9b3ef4133394468862c41717305a71db
Mohsin Sajjad <mohsin.sajjad@logiserves.com>
2026-05-05 18:19:34 +0500

    changes

--- config/custom.php
+++ config/custom.php
@@ -6,8 +6,8 @@ return [
     //syedshariqali302@gmail.com
     //faisalskynet3@yahoo.com
     //nazleenph@gmail.com
-    'SEND_MAIL' => 'uit.mohsin95@gmail.com',
 //    'SEND_MAIL' => 'nazleenph@gmail.com',
+    'SEND_MAIL' => 'nazleenph@gmail.com',
     'CODE_GIVER' => 'codeshipa1@gmail.com',
     'NEXMO_NO' => '17403400006',
 ];
