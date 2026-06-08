598c1bb93951d50bfd3c212db83701e100d1c63e
Mohsin Sajjad <mohsin.sajjad@logiserves.com>
2026-06-08 15:33:57 +0500

    changes

--- database/migrations/2026_06_08_000000_add_hr_employee_id_to_users.php
+++ database/migrations/2026_06_08_000000_add_hr_employee_id_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddHrEmployeeIdToUsers extends Migration
+class AddHrEmployeeIdToUsersV2 extends Migration
 {
     /**
      * Run the migrations.
